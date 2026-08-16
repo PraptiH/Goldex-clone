09d9db1049b60763fba0a9be11020ed95f5dc598
PraptiH <praptiantara1892@gmail.com>
2026-08-17 01:34:07 +0600

    Make the navbar responsive

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                     <Image width="120" height="31" src={logo} alt="" />
                 </div>
 
-                <div className="flex items-center gap-0.5">
+                <div className="hidden lg:flex items-center gap-0.5">
                     <button type="button" className="inline-flex items-center justify-center text-black/70 hover:text-black font-medium text-[18px] gap-1.5 h-7.5 px-3 cursor-pointer">
                         Products
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256" className="size-3.5 opacity-60"><path d="M216.49,104.49l-80,80a12,12,0,0,1-17,0l-80-80a12,12,0,0,1,17-17L128,159l71.51-71.52a12,12,0,0,1,17,17Z"></path></svg>
@@ -27,13 +27,17 @@ export default function Navbar() {
                     <span className="size-5">
                         <Image width="20" height="14" className="w-full h-full object-cover rounded-sm" src={en} alt="" />
                     </span>
-                    <p>EN</p>
+                    <p className="hidden md:inline-flex">EN</p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256" className="size-3.5 text-black/50 duration-200 opacity-60"><path d="M216.49,104.49l-80,80a12,12,0,0,1-17,0l-80-80a12,12,0,0,1,17-17L128,159l71.51-71.52a12,12,0,0,1,17,17Z"></path></svg>
                 </button>
 
 
-                <button className="font-medium text-white text-base bg-[#ace65a] hover:bg-[#9fd84f] cursor-pointer rounded-lg px-6 py-2.5 duration-200">Goldex for Business</button>
-                <button className="font-medium text-white text-base bg-black hover:opacity-90 cursor-pointer rounded-lg px-6 py-2.5 duration-200">Download App</button>
+                <button className="hidden lg:inline-flex font-medium text-white text-base bg-[#ace65a] hover:bg-[#9fd84f] cursor-pointer rounded-lg px-6 py-2.5 duration-200">Goldex for Business</button>
+                <button className="hidden md:inline-flex font-medium text-white text-base bg-black hover:opacity-90 cursor-pointer rounded-lg px-6 py-2.5 duration-200">Download App</button>
+
+                <button className="inline-flex lg:hidden ml-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256" aria-hidden="true" className="size-5"><path d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128ZM40,76H216a12,12,0,0,0,0-24H40a12,12,0,0,0,0,24ZM216,180H40a12,12,0,0,0,0,24H216a12,12,0,0,0,0-24Z"></path></svg>
+                </button>
             </div>
         </div>
     )
